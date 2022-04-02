b300523dad3a0dd0f532affbb2b85e9011206979
Smoffyy <kidcapcom08@gmail.com>
2022-04-02 14:02:15 -0500

    Added Detecting/Hitting Notes

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public bool canBePressed;
+    public KeyCode keyToPress;
+    public KeyCode altKeyToPress;
+
+    void Start() {
+
+    }
+    void Update() {
+        if(Input.GetKeyDown(keyToPress)) {
+            if(canBePressed) {
+                gameObject.SetActive(false);
+            }
+        }
+             // Alt Keybind
+            if(Input.GetKeyDown(altKeyToPress)) {
+            if(canBePressed) {   
+                gameObject.SetActive(false);
+            }
+        }
+    }    
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Active") {
+            canBePressed = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.tag == "Active") {
+            canBePressed = false;
+        }
+    }
+}
